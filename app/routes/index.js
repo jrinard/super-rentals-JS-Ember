a98f8794b2a332960b283293b3ad0c5dc083688b
joshua <jrinard@hotmail.com>
2017-01-08 00:12:44 -0800

    save rental

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,13 @@ export default Ember.Route.extend({ // exporting rentals  this is a MODEL HOOK
   model() { // same thing as writing model: function()
     return this.store.findAll('rental'); //refers to firebase data store to find all records of the type rental
   },
-    actions: {
+  actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
